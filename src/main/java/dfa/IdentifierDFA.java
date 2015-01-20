878f3955b8f8ef6c86408614ddff4227bf78ca7b
Manodasan W <mwignara@uwaterloo.ca>
2015-01-20 10:45:33 -0500

    Added Identifier DFA, and fixed package and directory name conflicts Added Test and MakeFile

---
+++ src/main/java/dfa/IdentifierDFA.java
@@ -0,0 +1,67 @@
+package dfa;
+
+import token.Token;
+import token.TokenType;
+
+/**
+ * Handles parsing of identifier
+ *
+ * Requirements:
+ *   Starts with Java letter which is $, _, a - z, A - Z
+ *   The rest of the characters are Java letters or digits.
+ */
+public class IdentifierDFA implements DFA {
+  private StringBuilder builder;
+  private Token token = null;
+
+  private enum states {ERROR, START, ACCEPT};
+  private states state;
+
+  public IdentifierDFA()
+  {
+    reset();
+  }
+
+  @Override
+  public void reset() {
+    state = states.START;
+    token = null;
+    builder = new StringBuilder();
+  }
+
+  // Checks if it is a Java letter which is $, _, a - z, A - Z
+  private boolean isJavaLetter(char c)
+  {
+    return c == '_' || c == '$' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+  }
+
+  @Override
+  public boolean consume(char c) {
+    switch (state) {
+      case ERROR:
+        break;
+      case START:
+        if (isJavaLetter(c)) {
+          state = states.ACCEPT;
+          builder.append(c);
+        }
+        else state = states.ERROR;
+        break;
+      case ACCEPT:
+        if (isJavaLetter(c) || (c >= '0' && c <= '9')) {
+          builder.append(c);
+        }
+        else {
+          state = states.ERROR;
+          token = new Token(builder.toString(), TokenType.IDENTIFIER);
+        }
+        break;
+    }
+    return state != states.ERROR;
+  }
+
+  @Override
+  public Token getToken() {
+    return token;
+  }
+}
---
+++ src/test/java/dfa/IdentifierDFATest.java
@@ -0,0 +1,124 @@
+package dfa;
+
+import dfa.IdentifierDFA;
+import org.junit.Test;
+import token.Token;
+import token.TokenType;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Basic tests to test the Identifier DFA.
+ */
+public class IdentifierDFATest {
+
+  @Test
+  public void testOneChar()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+
+    dfa.consume('d');
+    dfa.consume(' ');
+
+    Token result = dfa.getToken();
+    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
+    assertTrue(result.getLexeme().equals("d"));
+  }
+
+  @Test
+  public void testOneCharSpecial()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+
+    dfa.consume('$');
+    dfa.consume(' ');
+
+    Token result = dfa.getToken();
+    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
+    assertTrue(result.getLexeme().equals("$"));
+  }
+
+  @Test
+  public void testJavaString()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+    String javaString = "abcdefghijklmnoqprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$";
+
+    for(char c: javaString.toCharArray())
+    {
+      dfa.consume(c);
+    }
+    dfa.consume('\r');
+
+    Token result = dfa.getToken();
+    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
+    assertTrue(result.getLexeme().equals(javaString));
+  }
+
+  @Test
+  public void testJavaDigitsAndLetters()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+    String javaString = "test0123456789";
+
+    for(char c: javaString.toCharArray())
+    {
+      dfa.consume(c);
+    }
+    dfa.consume('\n');
+
+    Token result = dfa.getToken();
+    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
+    assertTrue(result.getLexeme().equals(javaString));
+  }
+
+  @Test
+  public void testJavaDigits()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+    String javaString = "01234test";
+
+    for(char c: javaString.toCharArray())
+    {
+      dfa.consume(c);
+    }
+    dfa.consume(' ');
+
+    assertTrue(dfa.getToken() == null);
+  }
+
+  @Test
+  public void testOneDigit()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+
+    dfa.consume('4');
+    dfa.consume(' ');
+
+    assertTrue(dfa.getToken() == null);
+  }
+
+  @Test
+  public void testEmpty()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+    assertTrue(dfa.getToken() == null);
+  }
+
+  @Test
+  public void testInvalid()
+  {
+    IdentifierDFA dfa = new IdentifierDFA();
+    String javaString = "ab3243^&abcd";
+
+    for(char c: javaString.toCharArray())
+    {
+      dfa.consume(c);
+    }
+    dfa.consume(' ');
+
+    Token result = dfa.getToken();
+    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
+    assertTrue(result.getLexeme().equals("ab3243"));
+  }
+}
