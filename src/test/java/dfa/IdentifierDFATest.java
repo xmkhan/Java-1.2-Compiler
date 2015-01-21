382aa49fb87c5586c6caa63c14690cc72f64b87d
Manodasan W <mwignara@uwaterloo.ca>
2015-01-21 11:02:27 -0500

    Changed tests to use @Before and assertEquals, and fixed folder structure issues

--- src/test/java/dfa/IdentifierDFATest.java
+++ src/test/java/dfa/IdentifierDFATest.java
@@ -1,44 +1,46 @@
 package dfa;
 
-import dfa.IdentifierDFA;
+import org.junit.Before;
 import org.junit.Test;
 import token.Token;
 import token.TokenType;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Basic tests to test the Identifier DFA.
  */
 public class IdentifierDFATest {
 
+  private IdentifierDFA dfa;
+
+  @Before
+  public void setUp() {
+    dfa = new IdentifierDFA();
+  }
+
   @Test
   public void testOneChar() {
-    IdentifierDFA dfa = new IdentifierDFA();
-
     dfa.consume('d');
     dfa.consume(' ');
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals("d"));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals("d", result.getLexeme());
   }
 
   @Test
   public void testOneCharSpecial() {
-    IdentifierDFA dfa = new IdentifierDFA();
-
     dfa.consume('$');
     dfa.consume(' ');
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals("$"));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals("$", result.getLexeme());
   }
 
   @Test
   public void testJavaString() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "abcdefghijklmnoqprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$";
 
     for(char c: javaString.toCharArray())
@@ -48,13 +50,12 @@ public class IdentifierDFATest {
     dfa.consume('\r');
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals(javaString));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals(javaString, result.getLexeme());
   }
 
   @Test
   public void testJavaDigitsAndLetters() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "test0123456789";
 
     for(char c: javaString.toCharArray())
@@ -64,13 +65,12 @@ public class IdentifierDFATest {
     dfa.consume('\n');
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals(javaString));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals(javaString, result.getLexeme());
   }
 
   @Test
   public void testJavaDigitsAndLettersProper() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "_test43 = 52;\n";
 
     for(char c: javaString.toCharArray()) {
@@ -78,13 +78,12 @@ public class IdentifierDFATest {
     }
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals("_test43"));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals("_test43", result.getLexeme());
   }
 
   @Test
   public void testJavaDigits() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "01234test";
 
     for(char c: javaString.toCharArray()) {
@@ -92,28 +91,24 @@ public class IdentifierDFATest {
     }
     dfa.consume(' ');
 
-    assertTrue(dfa.getToken() == null);
+    assertEquals(null, dfa.getToken());
   }
 
   @Test
   public void testOneDigit() {
-    IdentifierDFA dfa = new IdentifierDFA();
-
     dfa.consume('4');
     dfa.consume(' ');
 
-    assertTrue(dfa.getToken() == null);
+    assertEquals(null, dfa.getToken());
   }
 
   @Test
   public void testEmpty() {
-    IdentifierDFA dfa = new IdentifierDFA();
-    assertTrue(dfa.getToken() == null);
+    assertEquals(null, dfa.getToken());
   }
 
   @Test
   public void testInvalid() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "ab3243^&abcd";
 
     for(char c: javaString.toCharArray()) {
@@ -122,13 +117,12 @@ public class IdentifierDFATest {
     dfa.consume(' ');
 
     Token result = dfa.getToken();
-    assertTrue(result.getTokenType() == TokenType.IDENTIFIER);
-    assertTrue(result.getLexeme().equals("ab3243"));
+    assertEquals(TokenType.IDENTIFIER, result.getTokenType());
+    assertEquals("ab3243", result.getLexeme());
   }
 
   @Test
   public void testInvalidEscapes() {
-    IdentifierDFA dfa = new IdentifierDFA();
     String javaString = "\"abcdef33\'";
 
     for(char c: javaString.toCharArray()) {
@@ -136,6 +130,6 @@ public class IdentifierDFATest {
     }
     dfa.consume(' ');
 
-    assertTrue(dfa.getToken() == null);
+    assertEquals(null, dfa.getToken());
   }
 }
