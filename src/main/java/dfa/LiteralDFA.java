85770cd211acd5e9e144b209a6d34089e67c086c
Ali <alireza.pourhaj@gmail.com>
2015-01-21 01:56:03 -0500

    fixing indentation

--- src/main/java/dfa/LiteralDFA.java
+++ src/main/java/dfa/LiteralDFA.java
@@ -11,139 +11,139 @@ import java.util.Set;
  * Handles parsing of string literals
  */
 public class LiteralDFA implements DFA {
-    private StringBuilder builder;
-    private Token token = null;
+  private StringBuilder builder;
+  private Token token = null;
 
-    private enum LiteralType {SINGLE_QUOTE, DOUBLE_QUOTE}
+  private enum LiteralType {SINGLE_QUOTE, DOUBLE_QUOTE}
 
-    ;
-    private LiteralType literalType;
+  ;
+  private LiteralType literalType;
 
-    private enum states {ERROR, START, LITERAL, ESCAPE, ZERO_TO_THREE, ZERO_TO_SEVEN, ACCEPT}
+  private enum states {ERROR, START, LITERAL, ESCAPE, ZERO_TO_THREE, ZERO_TO_SEVEN, ACCEPT}
 
-    ;
-    private states state;
+  ;
+  private states state;
 
-    // used for detecting single quote literals with size > 1
-    private int length;
+  // used for detecting single quote literals with size > 1
+  private int length;
 
-    private final int MAX_ASCII = 127;
+  private final int MAX_ASCII = 127;
 
-    Set<Character> escapeCharacters;
+  Set<Character> escapeCharacters;
 
-    LiteralDFA() {
-        escapeCharacters = new HashSet<Character>(Arrays.asList('b', 't', 'n', 'f', 'r', '\'', '"'));
-        reset();
+  LiteralDFA() {
+    escapeCharacters = new HashSet<Character>(Arrays.asList('b', 't', 'n', 'f', 'r', '\'', '"'));
+    reset();
+  }
+
+  @Override
+  public void reset() {
+    length = 0;
+    token = null;
+    state = states.START;
+    builder = new StringBuilder();
+  }
+
+  @Override
+  public boolean consume(char c) {
+    // check if a single quote literal is longer than 1 character
+    if (literalType == LiteralType.SINGLE_QUOTE && (length > 1 || (length == 1 && c != '\''))) {
+      token = new Token(builder.toString(), TokenType.STR_LITERAL);
+      state = states.ERROR;
     }
 
-    @Override
-    public void reset() {
-        length = 0;
-        token = null;
-        state = states.START;
-        builder = new StringBuilder();
-    }
-
-    @Override
-    public boolean consume(char c) {
-        // check if a single quote literal is longer than 1 character
-        if (literalType == LiteralType.SINGLE_QUOTE && (length > 1 || (length == 1 && c != '\''))) {
-            token = new Token(builder.toString(), TokenType.STR_LITERAL);
-            state = states.ERROR;
-        }
-
-        switch (state) {
-            case ERROR:
-                break;
-            case START:
-                if (c == '\'') {
-                    builder.append(c);
-                    state = states.LITERAL;
-                    literalType = LiteralType.SINGLE_QUOTE;
-                } else if (c == '"') {
-                    builder.append(c);
-                    state = states.LITERAL;
-                    literalType = LiteralType.DOUBLE_QUOTE;
-                } else {
-                    state = states.ERROR;
-                }
-                break;
-            case LITERAL:
-                if (c == '\\') {
-                    builder.append(c);
-                    state = states.ESCAPE;
-                } else if (c <= MAX_ASCII && c != '\'' && c != '"') {
-                    // keep the same state
-                    builder.append(c);
-                    length++;
-                } else {
-                    endLiteral(c);
-                }
-                break;
-            case ESCAPE:
-                if (escapeCharacters.contains(c)) {
-                    length++;
-                    builder.append(c);
-                    state = states.LITERAL;
-                } else if (c >= '0' && c <= '3') {
-                    builder.append(c);
-                    state = states.ZERO_TO_THREE;
-                } else if (c > '3' && c <= '7') {
-                    builder.append(c);
-                    state = states.ZERO_TO_SEVEN;
-                } else {
-                    state = states.ERROR;
-                }
-                break;
-            case ZERO_TO_THREE:
-                if (c >= '0' && c <= '7') {
-                    builder.append(c);
-                    state = states.ZERO_TO_SEVEN;
-                } else if (c < MAX_ASCII) {
-                    builder.append(c);
-                    // \38 is the same as \3 followed by digit 8, so they count as 2 characters.
-                    length += 2;
-                    state = states.LITERAL;
-                } else {
-                    endLiteral(c);
-                }
-                break;
-            case ZERO_TO_SEVEN:
-                if (c >= '0' && c <= '7') {
-                    length++;
-                    builder.append(c);
-                    state = states.LITERAL;
-                } else if (c < MAX_ASCII) {
-                    builder.append(c);
-                    // \379 is the same as \37 followed by digit 9, so they count as 2 characters.
-                    length += 2;
-                    state = states.LITERAL;
-                } else {
-                    endLiteral(c);
-                }
-                break;
-            case ACCEPT:
-                state = states.ERROR;
-                token = new Token(builder.toString(), TokenType.STR_LITERAL);
-                break;
-        }
-
-        return state != states.ERROR;
-    }
-
-    private void endLiteral(char c) {
-        if ((c == '"' && literalType == LiteralType.DOUBLE_QUOTE) ||
-                (c == '\'' && literalType == LiteralType.SINGLE_QUOTE)) {
-            builder.append(c);
-            state = states.ACCEPT;
+    switch (state) {
+      case ERROR:
+        break;
+      case START:
+        if (c == '\'') {
+          builder.append(c);
+          state = states.LITERAL;
+          literalType = LiteralType.SINGLE_QUOTE;
+        } else if (c == '"') {
+          builder.append(c);
+          state = states.LITERAL;
+          literalType = LiteralType.DOUBLE_QUOTE;
         } else {
-            state = states.ERROR;
+          state = states.ERROR;
         }
+        break;
+      case LITERAL:
+        if (c == '\\') {
+          builder.append(c);
+          state = states.ESCAPE;
+        } else if (c <= MAX_ASCII && c != '\'' && c != '"') {
+          // keep the same state
+          builder.append(c);
+          length++;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ESCAPE:
+        if (escapeCharacters.contains(c)) {
+          length++;
+          builder.append(c);
+          state = states.LITERAL;
+        } else if (c >= '0' && c <= '3') {
+          builder.append(c);
+          state = states.ZERO_TO_THREE;
+        } else if (c > '3' && c <= '7') {
+          builder.append(c);
+          state = states.ZERO_TO_SEVEN;
+        } else {
+          state = states.ERROR;
+        }
+        break;
+      case ZERO_TO_THREE:
+        if (c >= '0' && c <= '7') {
+          builder.append(c);
+          state = states.ZERO_TO_SEVEN;
+        } else if (c < MAX_ASCII) {
+          builder.append(c);
+          // \38 is the same as \3 followed by digit 8, so they count as 2 characters.
+          length += 2;
+          state = states.LITERAL;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ZERO_TO_SEVEN:
+        if (c >= '0' && c <= '7') {
+          length++;
+          builder.append(c);
+          state = states.LITERAL;
+        } else if (c < MAX_ASCII) {
+          builder.append(c);
+          // \379 is the same as \37 followed by digit 9, so they count as 2 characters.
+          length += 2;
+          state = states.LITERAL;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ACCEPT:
+        state = states.ERROR;
+        token = new Token(builder.toString(), TokenType.STR_LITERAL);
+        break;
     }
 
-    @Override
-    public Token getToken() {
-        return token;
+    return state != states.ERROR;
+  }
+
+  private void endLiteral(char c) {
+    if ((c == '"' && literalType == LiteralType.DOUBLE_QUOTE) ||
+      (c == '\'' && literalType == LiteralType.SINGLE_QUOTE)) {
+      builder.append(c);
+      state = states.ACCEPT;
+    } else {
+      state = states.ERROR;
     }
+  }
+
+  @Override
+  public Token getToken() {
+    return token;
+  }
 
 }
--- src/test/java/dfa/LiteralDFATest.java
+++ src/test/java/dfa/LiteralDFATest.java
@@ -11,201 +11,201 @@ import static org.junit.Assert.assertEquals;
  * Basic tests to test the Literal DFA.
  */
 public class LiteralDFATest {
-    private LiteralDFA dfa = new LiteralDFA();
+  private LiteralDFA dfa = new LiteralDFA();
 
-    @Before
-    public void setUp() {
-        dfa.reset();
+  @Before
+  public void setUp() {
+    dfa.reset();
+  }
+
+  @Test
+  public void testEmptySingleQuoteLiteral() {
+    String literal = "''";
+
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testEmptySingleQuoteLiteral() {
-        String literal = "''";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "''");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testEmptyDoubleQuoteLiteral() {
+    String literal = "\"\"";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "''");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testEmptyDoubleQuoteLiteral() {
-        String literal = "\"\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"\"");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"\"");
+  @Test
+  public void testSingleQuoteInSingleQuotes() {
+    String literal = "'\\''";
+
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "'\\''");
+  }
 
-    @Test
-    public void testSingleQuoteInSingleQuotes() {
-        String literal = "'\\''";
+  @Test
+  public void testDoubleQuoteInDoubleQuotes() {
+    String literal = "\"\\\"\"";
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
-
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "'\\''");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testDoubleQuoteInDoubleQuotes() {
-        String literal = "\"\\\"\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"\\\"\"");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testSingleQuoteInDoubleQuotes() {
+    String literal = "\"\\'\"";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"\\\"\"");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testSingleQuoteInDoubleQuotes() {
-        String literal = "\"\\'\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"\\'\"");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testDoubleQuoteInSingleQuotes() {
+    String literal = "'\\\"'";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"\\'\"");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testDoubleQuoteInSingleQuotes() {
-        String literal = "'\\\"'";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "'\\\"'");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testLongSingleQuoteLiteral() {
+    dfa.consume('\'');
+    dfa.consume('a');
+    dfa.consume('b');
+    assertEquals(false, dfa.consume('\''));
+  }
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "'\\\"'");
+  @Test
+  public void testValidDoubleQuoteLiteral() {
+    String literal = "\"abc123\\'\\\"\"";
+
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testLongSingleQuoteLiteral() {
-        dfa.consume('\'');
-        dfa.consume('a');
-        dfa.consume('b');
-        assertEquals(false, dfa.consume('\''));
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"abc123\\'\\\"\"");
+  }
+
+  @Test
+  public void testEscapeCharactersInDoubleQuotes() {
+    String literal = "\"\\b\\t\\n\\f\\r\\'\\\"\"";
+
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testValidDoubleQuoteLiteral() {
-        String literal = "\"abc123\\'\\\"\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"\\b\\t\\n\\f\\r\\'\\\"\"");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testEscapeOctalInDoubleQuotes() {
+    String literal = "\"\\377a\"";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"abc123\\'\\\"\"");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testEscapeCharactersInDoubleQuotes() {
-        String literal = "\"\\b\\t\\n\\f\\r\\'\\\"\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "\"\\377a\"");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testEscapeCharacterInSingleQuotes() {
+    String literal = "'\t'";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"\\b\\t\\n\\f\\r\\'\\\"\"");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testEscapeOctalInDoubleQuotes() {
-        String literal = "\"\\377a\"";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "'\t'");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testEscapeOctalInSingleQuotes() {
+    String literal = "'\\377'";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "\"\\377a\"");
+    for (char c : literal.toCharArray()) {
+      dfa.consume(c);
     }
+    dfa.consume('\n');
 
-    @Test
-    public void testEscapeCharacterInSingleQuotes() {
-        String literal = "'\t'";
+    Token result = dfa.getToken();
+    assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
+    assertEquals(result.getLexeme(), "'\\377'");
+  }
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
+  @Test
+  public void testOctalEscapeOutOfRange() {
+    String literal = "'\\378'";
 
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "'\t'");
-    }
+    dfa.consume('\'');
+    dfa.consume('\\');
+    dfa.consume('3');
+    dfa.consume('7');
+    dfa.consume('8');
+    assertEquals(false, dfa.consume('\''));
 
-    @Test
-    public void testEscapeOctalInSingleQuotes() {
-        String literal = "'\\377'";
+    dfa.reset();
+    dfa.consume('\'');
+    dfa.consume('\\');
+    dfa.consume('3');
+    dfa.consume('8');
+    dfa.consume('7');
+    assertEquals(false, dfa.consume('\''));
 
-        for (char c : literal.toCharArray()) {
-            dfa.consume(c);
-        }
-        dfa.consume('\n');
-
-        Token result = dfa.getToken();
-        assertEquals(result.getTokenType(), TokenType.STR_LITERAL);
-        assertEquals(result.getLexeme(), "'\\377'");
-    }
-
-    @Test
-    public void testOctalEscapeOutOfRange() {
-        String literal = "'\\378'";
-
-        dfa.consume('\'');
-        dfa.consume('\\');
-        dfa.consume('3');
-        dfa.consume('7');
-        dfa.consume('8');
-        assertEquals(false, dfa.consume('\''));
-
-        dfa.reset();
-        dfa.consume('\'');
-        dfa.consume('\\');
-        dfa.consume('3');
-        dfa.consume('8');
-        dfa.consume('7');
-        assertEquals(false, dfa.consume('\''));
-
-        dfa.reset();
-        dfa.consume('\'');
-        dfa.consume('\\');
-        dfa.consume('4');
-        dfa.consume('7');
-        dfa.consume('7');
-        assertEquals(false, dfa.consume('\''));
-    }
+    dfa.reset();
+    dfa.consume('\'');
+    dfa.consume('\\');
+    dfa.consume('4');
+    dfa.consume('7');
+    dfa.consume('7');
+    assertEquals(false, dfa.consume('\''));
+  }
 }
