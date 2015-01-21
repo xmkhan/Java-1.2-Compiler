01894b2e45f720e0b63070956cec71c3ccaa107c
Ali <alireza.pourhaj@gmail.com>
2015-01-20 19:31:49 -0500

    initial literal dfa commit

--- src/main/java/DFA/LiteralDFA.java
+++ src/main/java/DFA/LiteralDFA.java
@@ -1,6 +1,7 @@
 package dfa;
 
 import token.Token;
+import token.TokenType;
 
 /**
  * Handles parsing of string literals
@@ -9,21 +10,111 @@ public class LiteralDFA implements DFA {
   private StringBuilder builder;
   private Token token = null;
 
-  private enum states {ERROR, START, OPEN_QUOTE ,ASCII, ZERO_TO_THREE, ONE_TO_SEVEN1, CLOSE_QUOTE, ACCEPT};
+  private enum LiteralType {SINGLE_QUOTE, DOUBLE_QUOTE};
+  private LiteralType literalType;
+
+  private enum states {ERROR, START, LITERAL, ESCAPE, ZERO_TO_THREE, ZERO_TO_SEVEN1,
+    ZERO_TO_SEVEN2, ACCEPT};
   private states state;
+
+  private int length;
+
+  private final int MAX_ASCII = 127;
 
   @Override
   public void reset() {
-
+    length = 0;
+    state = states.START;
+    builder = new StringBuilder();
+    token = null;
   }
 
   @Override
   public boolean consume(char c) {
-    return false;
+    if (literalType == LiteralType.SINGLE_QUOTE && length > 1 && c == '\'') {
+      state = states.ERROR;
+    }
+
+    switch(state) {
+      case ERROR:
+        break;
+      case START:
+        if (c == '\'') {
+          state = states.LITERAL;
+          literalType = LiteralType.SINGLE_QUOTE;
+        } else if (c == '"') {
+          state  = states.LITERAL;
+          literalType = LiteralType.DOUBLE_QUOTE;
+        } else {
+          state = states.ERROR;
+        }
+        break;
+      case LITERAL:
+        if (c == '\\') {
+          state = states.ESCAPE;
+        } else if (c <= MAX_ASCII) {
+          // keep the same states
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ESCAPE:
+        if (c == 'b' || c == 't' || c == 'r') {
+          state = states.LITERAL;
+        } else if (c >= '0' && c <= '3') {
+          state = states.ZERO_TO_THREE;
+        } else if (c > '3' && c <= '7') {
+          state = states.ZERO_TO_SEVEN2;
+        } else {
+          state = states.ERROR;
+        }
+        break;
+      case ZERO_TO_THREE:
+        if (c >= '0' && c <= '7') {
+          state = states.ZERO_TO_SEVEN2;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ZERO_TO_SEVEN1:
+        if (c >= '0' && c <= '7') {
+          state = states.ZERO_TO_SEVEN2;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ZERO_TO_SEVEN2:
+        if (c >= '0' && c <= '7') {
+          state = states.LITERAL;
+        } else {
+          endLiteral(c);
+        }
+        break;
+      case ACCEPT:
+        state = states.ERROR;
+        token = new Token(builder.toString(), TokenType.INT_LITERAL);
+        break;
+    }
+
+    if (c != '"' && c != '\'' && state != states.ERROR) {
+      builder.append(c);
+      length++;
+    }
+
+    return state != states.ERROR;
+  }
+
+  private void endLiteral(char c) {
+    if (c == '"' && literalType == LiteralType.DOUBLE_QUOTE ||
+            c == '\'' && literalType == LiteralType.SINGLE_QUOTE) {
+      state = states.ACCEPT;
+    } else {
+      state = states.ERROR;
+    }
   }
 
   @Override
   public Token getToken() {
-    return null;
+    return token;
   }
 }
