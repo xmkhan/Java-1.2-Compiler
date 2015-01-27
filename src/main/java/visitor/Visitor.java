f4dcaa703075d248fb0a431359a1c26bc3c516fb
Manodasan W <mwignara@uwaterloo.ca>
2015-01-27 00:25:03 -0500

    Changes to rules to support 1 mandatory class

--- src/main/java/visitor/Visitor.java
+++ src/main/java/visitor/Visitor.java
@@ -52,8 +52,6 @@ public interface Visitor {
 
   public void visit(ForStatement token);
 
-  public void visit(TypeDeclarations token);
-
   public void visit(ConditionalAndExpression token);
 
   public void visit(TypeImportOnDemandDeclaration token);
