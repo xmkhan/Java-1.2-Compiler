fe080a5276adf26b7e9cae9cd68bfd910130ef79
Manodasan W <mwignara@uwaterloo.ca>
2015-03-12 13:20:03 -0400

    Adding visitor checks and fixed unaryexpression rule

--- src/main/java/token/ArrayAccess.java
+++ src/main/java/token/ArrayAccess.java
@@ -7,22 +7,43 @@ import java.util.ArrayList;
 
 public class ArrayAccess extends Token {
 
+  public Name name;
+  public Primary primary;
+  public Expression expression;
+
   public ArrayAccess(ArrayList<Token> children) {
     super("", TokenType.ArrayAccess, children);
+    for (Token token : children) {
+      assignType(token);
+    }
+  }
+
+  private void assignType(Token token) {
+    if (token instanceof Name) {
+      name = (Name) token;
+    } else if (token instanceof Expression) {
+      expression = (Expression) token;
+    } else if (token instanceof Primary) {
+      primary = (Primary) token;
+    }
+  }
+
+  public boolean isAccessOnPrimary() {
+    return primary != null;
   }
 
   public void accept(Visitor v) throws VisitorException {
-    for (Token token : children) {
-      token.accept(v);
-    }
+    if(name != null) name.accept(v);
+    if(primary != null) primary.accept(v);
+    if(expression != null) expression.accept(v);
     v.visit(this);
   }
 
   @Override
   public void acceptReverse(Visitor v) throws VisitorException {
     v.visit(this);
-    for (Token token : children) {
-      token.acceptReverse(v);
-    }
+    if(name != null) name.acceptReverse(v);
+    if(primary != null) primary.acceptReverse(v);
+    if(expression != null) expression.acceptReverse(v);
   }
 }
--- src/main/java/token/MethodInvocation.java
+++ src/main/java/token/MethodInvocation.java
@@ -7,23 +7,52 @@ import java.util.ArrayList;
 
 public class MethodInvocation extends Token {
 
+  public Name name;
+  public ArgumentList argumentList;
+  public Primary primary;
+  public Token identifier;
+
   public MethodInvocation(ArrayList<Token> children) {
     super("", TokenType.MethodInvocation, children);
+    for (Token token : children) {
+      assignType(token);
+    }
+  }
+
+  private void assignType(Token token) {
+    if (token instanceof Name) {
+      name = (Name) token;
+    } else if (token.getTokenType() == TokenType.IDENTIFIER) {
+      identifier = token;
+    } else if (token instanceof ArgumentList) {
+      argumentList = (ArgumentList) token;
+    } else if (token instanceof Primary) {
+      primary = (Primary) token;
+    }
+  }
+
+  public boolean isOnPrimary() {
+    return primary != null;
+  }
+
+  public boolean hasArguments() {
+    return argumentList != null;
   }
 
   @Override
   public void accept(Visitor v) throws VisitorException {
-    for (Token token : children) {
-      token.accept(v);
-    }
+    if (name != null) name.accept(v);
+    if (primary != null) primary.accept(v);
+    if (identifier != null) identifier.accept(v);
+    if (argumentList != null) argumentList.accept(v);
     v.visit(this);
   }
 
   @Override
   public void acceptReverse(Visitor v) throws VisitorException {
     v.visit(this);
-    for (Token token : children) {
-      token.acceptReverse(v);
-    }
-  }
+    if (name != null) name.acceptReverse(v);
+    if (primary != null) primary.acceptReverse(v);
+    if (identifier != null) identifier.acceptReverse(v);
+    if (argumentList != null) argumentList.acceptReverse(v);  }
 }
