ea93fa035f121dda3b729d8fd0bfaa2576154760
Mohammad Khan <ms22khan@uwaterloo.ca>
2015-04-05 22:27:26 -0400

    sdjfsdfj

--- src/main/java/visitor/CodeGenerationVisitor.java
+++ src/main/java/visitor/CodeGenerationVisitor.java
@@ -1076,7 +1076,8 @@ public class CodeGenerationVisitor extends BaseVisitor {
     }
     // Set the vtpr and classId
     output.println("pop eax");
-    output.println(String.format("mov dword [eax], [__vtable__%s]", classDeclaration.getAbsolutePath()));
+    output.println(String.format("mov ebx, [__vtable__%s]", classDeclaration.getAbsolutePath()));
+    output.println("mov [eax], ebx");
     output.println("mov eax, [eax]");
     output.println(String.format("mov dword [eax], %d", classDeclaration.classId));
     offset = 0;
